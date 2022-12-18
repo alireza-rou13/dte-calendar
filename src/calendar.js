42c886416ae447f15afad411983ee0222626f40d
Alireza Rou <45900246+alireza-rou13@users.noreply.github.com>
2022-12-18 13:29:57 +0330

    first commit after whole coding

--- src/calendar.js
+++ src/calendar.js
@@ -5,10 +5,15 @@ import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
 import moment from 'jalali-moment'
 import axios from "axios";
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button';
+
 import tippy from 'tippy.js';
 import 'tippy.js/animations/scale.css';
-import 'tippy.js/themes/light-border.css';
+import 'tippy.js/dist/tippy.css';
 
+import 'bootstrap/dist/css/bootstrap.rtl.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { ReactComponent as RightIcon } from "./right.svg"
 import { ReactComponent as LeftIcon } from "./left.svg"
@@ -22,22 +27,27 @@ export default function Calendar({ ...props }) {
     const [isLoadComplete, setIsLoadComplete] = useState(false)
     const [hlpMonthStartDayDate, setHlpMonthStartDayDate] = useState()
     const [hlpMonthEndDayDate, setHlpMonthEndDayDate] = useState()
-
+    const [events , fetch_events] = useState()
     const jalaiDate = moment(new Date(curentActiveMonth)).locale('fa').format('YYYY/MM/DD')
     const month = resolveMonthName(jalaiDate.split('/')[1])
     const year = jalaiDate.split('/')[0]
+
+
     const fetchData = async () => {
         let res = await axios.get("http://share.morsalat.ir/programs");
-        return res.data;
+        fetch_events(res.data);
     }
+    
 
     useEffect(() => {
+        fetchData()
         calculateCalendarDays(curentActiveMonth)
     }, [])
 
     const calculateCalendarDays = (activeDate) => {
+
         setIsLoadComplete(false)
-        
+
         const jalaiMonthStartDate = moment(new Date(activeDate)).locale('fa').format('YYYY/MM/01')
         const hlpDate = new Date(moment.from(jalaiMonthStartDate, 'fa', 'YYYY/MM/DD').format('YYYY-MM-DD'))
         const calendarStartDate = hlpDate
@@ -80,7 +90,6 @@ export default function Calendar({ ...props }) {
     const calendar = document.getElementsByClassName('fc-daygrid-day')
 
     const dayCellDidMount = (hlpMonthStartDayDate, hlpMonthEndDayDate) => {
-
         for (let i = 0; i < calendar.length; i++) {
             calendar[i].className = calendar[i].className.replace(' fc-day-other', '')
             if ((new Date(calendar[i].getAttribute('data-date'))).getTime() >= new Date(hlpMonthEndDayDate).getTime() ||
@@ -107,28 +116,49 @@ export default function Calendar({ ...props }) {
 
         return calendarDom ? calendarDom.getApi() : null;
     }
+    const [show, setShow] = useState(false);
+    var [modalbody,setbody] = useState("empty");
+    var [modaltitle,settitle] = useState("empty");
+    var [modalauthor,setauthor] = useState("empty");
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const handle_event = (info) => {
+        settitle(info.title);
+        setauthor(info.extendedProps.field_full_name)
+        const _start=moment(info.start).locale('fa').format('D MMMM ساعت hh:mm');
+        const _end=moment(info.end).locale('fa').format('hh:mm');
+        var ap_text= info.extendedProps.approved==="1" ? "تایید شده است." : "تایید نشده است." ;
+        var ap_icon=info.extendedProps.approved==="0" ? `  <i class="bi bi-exclamation-triangle-fill"></i>  ` : `  <i class="bi bi-check-square-fill"></i>  ` ;
+        var ap_class=info.extendedProps.approved ? "alert-success" : "alert-danger" ;
+        const _approve=`<div class="alert ${ap_class}" role="alert">${ap_icon}زمان و مکان این رویداد توسط ناظر  ${ap_text} </div>`
+        const _alert=`<div class="alert alert-success" role="alert">  این برنامه ${_start} در ${info.extendedProps.field_place}  به صورت ${info.extendedProps.field_type_metting} آغاز و در ساعت   ${_end} به پایان می رسد.</div>`
+        setbody(_alert+_approve+info.extendedProps.description);
+    }
 
     return (
         <>
             {isLoadComplete &&
                 <>
+
                     <div className='claendar-toolbar'>
-                        <div
-                        className='toolbar-arrow-right'
-                        onClick={() =>{
-                            let next = view === "dayGrid" ? "timeGridWeek" : "dayGrid"
-                            changeView((next))
-                         }
-                            
-                          }
-                        >
-                            salam
-                        </div>
+                        {/*<div*/}
+                        {/*className='toolbar-arrow-right'*/}
+                        {/*onClick={() =>{*/}
+                        {/*    let next = view === "dayGrid" ? "timeGridWeek" : "dayGrid"*/}
+                        {/*    changeView((next))*/}
+                        {/* }*/}
+                        {/*    */}
+                        {/*  }*/}
+                        {/*>*/}
+                        {/*    نمایش هفته*/}
+                        {/*</div>*/}
                         <div
                             className='toolbar-arrow-right'
                             onClick={showPreviousMonth}
                         >
                             <RightIcon />
+                            ماه قبل
                         </div>
                         <h1>{month} {year}</h1>
                         <div className='left-toolbar'>
@@ -142,37 +172,58 @@ export default function Calendar({ ...props }) {
                                 className='toolbar-arrow-left'
                                 onClick={showNextMonth}
                             >
+                                ماه بعد
                                 <LeftIcon />
                             </div>
                         </div>
                     </div>
-                    
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title><h4 className="modal-title">{modaltitle}</h4></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body><div dangerouslySetInnerHTML={{ __html: modalbody }} /></Modal.Body>
+                        <Modal.Footer>
+                            <div className="alert alert-info alert-footer" role="alert">
+                                ثبت شده توسط :{modalauthor}
+                            </div>
+                            <Button variant="secondary" onClick={handleClose}>
+                                بستن
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
                     <FullCalendar
                         initialView={view}
                         plugins={[dayGridPlugin,timeGridPlugin]}
                         ref={calendarRef}
                         locale={faLocale}
+                        themeSystem='bootstrap5'
                         visibleRange={range}
                         dayCellDidMount={() => dayCellDidMount(hlpMonthStartDayDate, hlpMonthEndDayDate)}
                         headerToolbar={false}
-                        eventClick= {(event) {
-                            var modal = $("#schedule-edit");
-                            modal.modal();
+                        eventClick= {(info)=> {
+                            console.log(info.event.extendedProps)
+                            handle_event(info.event)
+                            handleShow(true)
                         }}
                         eventDidMount={(info) => {
-                            //console.log(info.event);
+                            // console.log(info.event);
+                            console.log(range)
+                            const msg= info.event.extendedProps.approved === "1" ? "✅ (تایید شده)" : "⚠️(در دست بررسی)"
                             tippy(info.el, {
-                              animation: 'scale',
-                              content:info.event.extendedProps.description,
+                              placement: 'left',
+                              animation: 'fade',
+                              duration:[500,0],
+                              content:msg+"<br>"+info.event.title,
                               allowHTML: true,
                               interactive: true,
                               interactiveDebounce: 75,
                               theme:'light-border',
                             });
                           }}
-                        events = {fetchData}
+                        events = {events}
                         {...props}
                     />
+
                 </>
             }
         </>
